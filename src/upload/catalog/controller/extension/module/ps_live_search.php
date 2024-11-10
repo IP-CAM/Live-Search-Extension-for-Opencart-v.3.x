cf741223ae54c585876eccd6130f10c7b0734348
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-10 10:33:44 +0100

    JSON_UNESCAPED_SLASHES and &amp; to & bug fix

--- src/upload/catalog/controller/extension/module/ps_live_search.php
+++ src/upload/catalog/controller/extension/module/ps_live_search.php
@@ -54,7 +54,7 @@ class ControllerExtensionModulePsLiveSearch extends Controller
                 }
 
                 $json['products']['data'][] = [
-                    'href' => $this->url->link('product/product', 'product_id=' . $productResult['product_id']),
+                    'href' => str_replace('&amp;', '&', $this->url->link('product/product', 'product_id=' . $productResult['product_id'])),
                     'name' => strip_tags($productResult['name']),
                     'description' => $description,
                     'price' => $price,
@@ -67,67 +67,7 @@ class ControllerExtensionModulePsLiveSearch extends Controller
             }
         }
 
-        if ($this->config->get('module_ps_live_search_category_status')) {
-            $categoryResults = $this->model_extension_module_ps_live_search->getCategories($search);
-
-            foreach ($categoryResults as $categoryResult) {
-                if ($this->config->get('module_ps_live_search_category_image') && is_file(DIR_IMAGE . html_entity_decode($categoryResult['image'], ENT_QUOTES, 'UTF-8'))) {
-                    $thumb = $this->model_tool_image->resize(html_entity_decode($categoryResult['image'], ENT_QUOTES, 'UTF-8'), $this->config->get('module_ps_live_search_category_image_width'), $this->config->get('module_ps_live_search_category_image_height'));
-                } else {
-                    $thumb = '';
-                }
-
-                $name = [$categoryResult['names']];
-
-                if ($categoryResult['name']) {
-                    $name[] = $categoryResult['name'];
-                }
-
-                $paths = array_filter([$categoryResult['paths'], $categoryResult['category_id']]);
-
-                $json['categories']['data'][] = [
-                    'href' => $this->url->link('product/category', 'path=' . implode('_', $paths)),
-                    'name' => implode(' > ', array_filter($name)),
-                    'thumb' => $thumb,
-                    'thumb_width' => $this->config->get('module_ps_live_search_category_image_width'),
-                    'thumb_height' => $this->config->get('module_ps_live_search_category_image_height'),
-                ];
-            }
-        }
-
-        if ($this->config->get('module_ps_live_search_manufacturer_status')) {
-            $manufacturerResults = $this->model_extension_module_ps_live_search->getManufacturers($search);
-
-            foreach ($manufacturerResults as $manufacturerResult) {
-
-                if ($this->config->get('module_ps_live_search_manufacturer_image') && is_file(DIR_IMAGE . html_entity_decode($manufacturerResult['image'], ENT_QUOTES, 'UTF-8'))) {
-                    $thumb = $this->model_tool_image->resize(html_entity_decode($manufacturerResult['image'], ENT_QUOTES, 'UTF-8'), $this->config->get('module_ps_live_search_manufacturer_image_width'), $this->config->get('module_ps_live_search_manufacturer_image_height'));
-                } else {
-                    $thumb = '';
-                }
-
-                $json['manufacturers']['data'][] = [
-                    'href' => $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturerResult['manufacturer_id']),
-                    'name' => $manufacturerResult['name'],
-                    'thumb' => $thumb,
-                    'thumb_width' => $this->config->get('module_ps_live_search_manufacturer_image_width'),
-                    'thumb_height' => $this->config->get('module_ps_live_search_manufacturer_image_height'),
-                ];
-            }
-        }
-
-        if ($this->config->get('module_ps_live_search_information_status')) {
-            $informationResults = $this->model_extension_module_ps_live_search->getInformations($search);
-
-            foreach ($informationResults as $informationResult) {
-                $json['informations']['data'][] = [
-                    'href' => $this->url->link('information/information', 'information_id=' . $informationResult['information_id']),
-                    'name' => $informationResult['title']
-                ];
-            }
-        }
-
         $this->response->addHeader('Content-Type: application/json');
-        $this->response->setOutput(json_encode($json));
+        $this->response->setOutput(json_encode($json, JSON_UNESCAPED_SLASHES));
     }
 }
